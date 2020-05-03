062c89638f155c488f0b79db61e9c16839e3dadf
didier <didier.rda@gmail.com>
2020-05-03 14:30:48 -0300

    add filter.hs

--- exd_exs/filter_even.hs
+++ exd_exs/filter_even.hs
@@ -1,2 +1,9 @@
 -- Double even
-doubleEven :: [Integer] -> []
+filterEven :: [Integer] -> [Integer]
+filterEven [] = []
+filterEven (x:xs) = if  mod x 2 == 0  then filterEven xs else x:(filterEven xs)
+
+--  if x then y else z (then e else do mesmo tipo, y e z)
+--  sempre retorna algo (é uma função)
+--  predicado: operação que retorna um boleano
+--  filter isEven 
